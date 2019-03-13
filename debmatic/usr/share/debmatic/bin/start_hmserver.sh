62e5769ae3970628bea708dd676d68d1ab2831e2
Alexander Reinert <alex@areinert.de>
2019-03-13 22:07:38 +0100

    Use special version of HMServer if no HmIP capable radio module is attached

--- debmatic/usr/share/debmatic/bin/start_hmserver.sh
+++ debmatic/usr/share/debmatic/bin/start_hmserver.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 if [ -z "$HM_HMIP_DEV" ]; then
-  HM_HMIP_DEVNODE=/dev/mmd_hmip
+  HMSERVER_BIN="HMServer"
+  HMSERVER_ARGS="/etc/HMServer.conf"
+else
+  HMSERVER_BIN="HMIPServer"
+  HMSERVER_ARGS="/var/run/crRFD.conf /etc/HMServer.conf"
+  sed "s|^Adapter\.1\.Port=.*$|Adapter.1.Port=${HM_HMIP_DEVNODE}|" /etc/crRFD.conf > /var/run/crRFD.conf
 fi
 
 sed "s|^Adapter\.1\.Port=.*$|Adapter.1.Port=${HM_HMIP_DEVNODE}|" /etc/crRFD.conf > /var/run/crRFD.conf
-/usr/bin/java -Xmx128m -Dlog4j.configuration=file:///etc/config/log4j.xml -Dfile.encoding=ISO-8859-1 -Dgnu.io.rxtx.SerialPorts=$HM_HMIP_DEVNODE -jar /opt/HMServer/HMIPServer.jar /var/run/crRFD.conf /etc/HMServer.conf &
+/usr/bin/java -Xmx128m -Dlog4j.configuration=file:///etc/config/log4j.xml -Dfile.encoding=ISO-8859-1 -Dgnu.io.rxtx.SerialPorts=$HM_HMIP_DEVNODE -jar /opt/HMServer/$HMSERVER_BINHMIP.jar $HMSERVER_ARGS &
 echo $! > /var/run/HMIPServer.pid
 
 for i in {1..120}; do
