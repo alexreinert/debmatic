4b3aa11e5a907f50fb0c5ac6208c0f2b381ee11a
Alexander Reinert <alex@areinert.de>
2019-03-13 22:14:07 +0100

    Fixed typo in HMServer startup script

--- debmatic/usr/share/debmatic/bin/start_hmserver.sh
+++ debmatic/usr/share/debmatic/bin/start_hmserver.sh
@@ -10,7 +10,7 @@ else
 fi
 
 sed "s|^Adapter\.1\.Port=.*$|Adapter.1.Port=${HM_HMIP_DEVNODE}|" /etc/crRFD.conf > /var/run/crRFD.conf
-/usr/bin/java -Xmx128m -Dlog4j.configuration=file:///etc/config/log4j.xml -Dfile.encoding=ISO-8859-1 -Dgnu.io.rxtx.SerialPorts=$HM_HMIP_DEVNODE -jar /opt/HMServer/$HMSERVER_BINHMIP.jar $HMSERVER_ARGS &
+/usr/bin/java -Xmx128m -Dlog4j.configuration=file:///etc/config/log4j.xml -Dfile.encoding=ISO-8859-1 -Dgnu.io.rxtx.SerialPorts=$HM_HMIP_DEVNODE -jar /opt/HMServer/$HMSERVER_BIN.jar $HMSERVER_ARGS &
 echo $! > /var/run/HMIPServer.pid
 
 for i in {1..120}; do
