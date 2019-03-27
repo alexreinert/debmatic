8233177f2219468b02e1fb3f8d19e9fcb2e55806
Alexander Reinert <alex@areinert.de>
2019-03-27 22:58:07 +0100

    Added Homematic check_mk addon

--- README.md
+++ README.md
@@ -70,6 +70,7 @@ sudo debmatic-backup
 Addons können per apt installiert werden. Aktuell exitieren folgende Addons:
 * [CUxD](http://www.cuxd.de/) (Paketname cuxd)
 * [JP HB Devices](https://github.com/jp112sdl/JP-HB-Devices-addon) (Paketname jp-hb-devices)
+* [Homematic check_mk addon](https://github.com/alexreinert/homematic_check_mk) (Paketname homematic-check-mk)
 
 ### Restore
 Backups können über die WebUI eingespielt werden.
---
+++ addons/homematic-check-mk/create_homematic-check-mk.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ADDON_VERSION=1.3
+
+PKG_BUILD=1
+
+CURRENT_DIR=$(pwd)
+WORK_DIR=$(mktemp -d)
+
+PKG_VERSION=$ADDON_VERSION-$PKG_BUILD
+
+cd $WORK_DIR
+
+TARGET_DIR=$WORK_DIR/homematic-check-mk-$PKG_VERSION
+
+mkdir -p $TARGET_DIR
+
+cp -a $CURRENT_DIR/homematic-check-mk/* $TARGET_DIR 
+
+for file in $TARGET_DIR/DEBIAN/*; do
+  sed -i "s/{PKG_VERSION}/$PKG_VERSION/g" $file
+done
+
+cd $WORK_DIR
+
+dpkg-deb --build homematic-check-mk-$PKG_VERSION
+
+cp homematic-check-mk-*.deb $CURRENT_DIR
+
+echo "Please clean-up the work dir temp folder $WORK_DIR, e.g. by doing rm -R $WORK_DIR"
+
