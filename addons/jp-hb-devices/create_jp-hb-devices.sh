e9120f438769e6da578688a701b02fa1fc719b68
Alexander Reinert <alex@areinert.de>
2019-08-05 20:46:41 +0200

    Updated JP-HB-Devices Addon to 2.11

--- addons/jp-hb-devices/create_jp-hb-devices.sh
+++ addons/jp-hb-devices/create_jp-hb-devices.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ADDON_VERSION=2.10
+ADDON_VERSION=2.11
 
 ARCHIVE_TAG="$ADDON_VERSION"
 
 ADDON_DOWNLOAD_URL="https://github.com/jp112sdl/JP-HB-Devices-addon/archive/$ARCHIVE_TAG.tar.gz"
 
-PKG_BUILD=6
+PKG_BUILD=7
 
 CURRENT_DIR=$(pwd)
 WORK_DIR=$(mktemp -d)
