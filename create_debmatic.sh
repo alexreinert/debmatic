a7d1b93c36a39856c254969b3e1fe9472b838969
Alexander Reinert <alex@areinert.de>
2022-05-10 12:56:34 +0200

    Updated to CCU3 firmware 3.63.9

--- create_debmatic.sh
+++ create_debmatic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CCU_VERSION="3.63.8"
+CCU_VERSION="3.63.9"
 
-ARCHIVE_TAG="ec9405105f706cc3e5e18e22400527d11348f75e"
+ARCHIVE_TAG="45b38865f6b60f16f825b75f0bdc8a9738831ee0"
 OCCU_DOWNLOAD_URL="https://github.com/eq-3/occu/archive/$ARCHIVE_TAG.tar.gz"
 
 CCU_DOWNLOAD_SPLASH_URL="https://www.eq-3.de/service/downloads.html"
@@ -15,7 +15,7 @@ JP_HB_DEVICES_ADDON_DOWNLOAD_URL="https://github.com/jp112sdl/JP-HB-Devices-addo
 HB_TM_DEVICES_ADDON_ARCHIVE_TAG="ab7bdeba2c180d5b6fc453a010d4ee2b882a929d"
 HB_TM_DEVICES_ADDON_DOWNLOAD_URL="https://github.com/TomMajor/SmartHome/archive/$HB_TM_DEVICES_ADDON_ARCHIVE_TAG.tar.gz"
 
-PKG_BUILD=92
+PKG_BUILD=93
 
 CURRENT_DIR=$(pwd)
 WORK_DIR=$(mktemp -d)
